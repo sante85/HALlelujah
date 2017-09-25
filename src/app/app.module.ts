ce2d79beb50e063f301a02b98afb6ec32ee4e85a
Jasper Uyttebroek <jasper.uyttebroek@gmail.com>
2017-09-25 22:45:10 +0200

    npm files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-import {HallelujahModule} from './hallelujah/hallelujah.module';
+import {HallelujahModule} from './hallelujah/index';
 
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
